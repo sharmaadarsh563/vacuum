74de6f57c966a01eb526aac1156b74746e9547d5
Adarsh Sharma <sharmaadarsh563@gmail.com>
2018-10-22 02:23:38 +0530

    tests

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,13 +3,20 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config=None, **kwargs):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    if kwargs.get('db_path'):
+        app.config['DATABASE'] = kwargs.get('db_path')
+    else:
+        app.config['DATABASE'] = os.path.join(
+            app.instance_path, 'flaskr.sqlite')
+
+    app.config['TESTING'] = kwargs.get('TESTING', False)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
